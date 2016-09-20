6b8b426b298941554379955b47bbc0b27a30543f
James Milani <james.milani@gmail.com>
2016-09-20 14:34:10 -0700

    Flash Messages moved to partial.

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
 
   get '/skills/:skill_id/intents/:id/fields_and_dialogs',
     to: 'intents#fields_and_dialogs',
-    as:'fields_and_dialogs'
+    as: 'fields_and_dialogs'
 end
