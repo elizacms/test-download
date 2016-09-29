3559ffb48cfe31486225d07c1013298a2a91e166
George <george@boost-media.com>
2016-09-29 11:29:45 -0700

    Cleanup

--- app/assets/javascripts/js_grid_export.js
+++ app/assets/javascripts/js_grid_export.js
@@ -1,35 +1,8 @@
-function headerValues(){
-  return $( '#fields' ).data( 'JSGrid' )
-                       .fields
-                       .filter( function( d ){
-                         return d.name != '';
-                       })
-                       .map( function( d ){
-                         return d.name
-                       })
-}
-
-function dataObjects(){
-  return $( '#fields' ).data( 'JSGrid' ).data
-}
-
-function createJSONForSingle( object ){
-  var hash = {};
-  
-  headerValues().map( function( key ){
-    hash[ key ] = object[ key ]
-  });
-
-  return hash;
-}
-
 function createJSON(){
-  objects = dataObjects().map( function( o ){
-    return createJSONForSingle( o )
-  });
+  var data = $("#fields").jsGrid("option", "data");
 
   var top = { id:     intent.name ,
-              fields: objects     };
+              fields: data       };
 
   return JSON.stringify( top, null, 2 );
 }
