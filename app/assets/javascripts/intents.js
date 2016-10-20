5bf6179f3c057a055031e82b9bc7a03a024d8c3d
James Milani <james.milani@gmail.com>
2016-10-20 11:40:34 -0700

    Refactor js for turbolinks using .on('turbolinks:load', function(){}).

--- app/assets/javascripts/intents.js
+++ app/assets/javascripts/intents.js
@@ -1,3 +1,18 @@
+$(document).on('turbolinks:load', function() {
+  if ( $('input[id=intent_requires_authorization]').attr('checked') == 'checked' ){
+    $('#authList').collapse('show');
+  }
+  else {
+    $('#authList').collapse('hide');
+  }
+
+  $('input[id=intent_requires_authorization]').click(
+    function(){
+      $('#authList').collapse('toggle');
+    }
+  );
+});
+
 function saveMturkResponse( e ){
   e.preventDefault();
 
