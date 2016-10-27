704806053d305f965e7558f67e1025b5efc6c3cb
James Milani <james.milani@gmail.com>
2016-10-27 16:22:09 -0700

    Move the CSV button and take it out of the form. OSSM-34

--- app/assets/javascripts/export_csv.js
+++ app/assets/javascripts/export_csv.js
@@ -1,5 +1,5 @@
 function initCSVButton(){
-  $( '.dialog button.export-csv' ).click(function( event ){
+  $( 'button.export-csv' ).click(function( event ){
     event.preventDefault();
     getCSV();
   });
@@ -7,6 +7,6 @@ function initCSVButton(){
 
 function getCSV(){
   var intent_id = $( 'form' ).children( 'input[name="intent-id"]' ).val();
-  
+
   window.location.assign( '/dialogue_api/csv?intent_id=' + intent_id );
 };
