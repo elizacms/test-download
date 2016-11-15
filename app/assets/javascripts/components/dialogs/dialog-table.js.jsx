c06cfdf8b2e365e6d9fce462b45cc34c5152c173
James Milani <james.milani@gmail.com>
2016-11-15 14:58:37 -0800

    Clean up the header widths a bit for dialogs page.

--- app/assets/javascripts/components/dialogs/dialog-table.js.jsx
+++ app/assets/javascripts/components/dialogs/dialog-table.js.jsx
@@ -9,12 +9,12 @@ var DialogTable = React.createClass({
         <table className='dialogs'>
           <tbody>
             <tr>
-              <th className='row10'>&nbsp;Priority</th>
-              <th className='row30'>AneedA Says</th>
-              <th className='row30'>Rules</th>
+              <th className='row4'>&nbsp;Priority</th>
+              <th className='row20'>AneedA Says</th>
+              <th className='row20'>Rules</th>
               <th className='row10'>Awaiting Field</th>
-              <th className='row5'></th>
-              <th className='row5'></th>
+              <th className='row3'>Options</th>
+              <th className='row3'></th>
             </tr>
             {
               this.props.data.map(function(dialog, index){
