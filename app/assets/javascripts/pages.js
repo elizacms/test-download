cf0ec3d60707ebf043581da3b323c5be91524afa
James Milani <james.milani@gmail.com>
2017-01-04 11:46:37 -0800

    Changing out a query in mid-stream will clear out the queries. Changing the skill clears the page and fills in the wrapper.

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,4 +1,10 @@
-function fillWrapperQuery(){
+function fillWrapperQueryAndClearPrevious(){
     var intent = $('select#intents').val();
     $('#wrapper_query').val(intent);
+
+    $('.notes').removeClass('in');
+    $('code.json').html('');
+    $('#wrapper_query').closest('div').nextAll('div').each(function(){
+      $(this).find('.text-input').val('')
+    });
 }
