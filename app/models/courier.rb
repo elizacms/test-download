0ab3f3791dfec034ababffc2c5af2c1285b6cf6e
James Milani <james.milani@gmail.com>
2017-02-06 14:00:41 -0800

    Better error message for test-queries json generator error.

--- app/models/courier.rb
+++ app/models/courier.rb
@@ -1,20 +1,20 @@
 class Courier
   class <<self
     def post_request( url, body )
+      start_time = Time.now
+      res = HTTParty.post( url, body: body, headers: { 'Content-Type' => 'application/json' } )
+      end_time = Time.now
+
       begin
-        start_time = Time.now
-        response = JSON.pretty_generate(
-          HTTParty.post( url, body: body, headers: { 'Content-Type' => 'application/json' } )
-        )
-        end_time = Time.now
+        res_json = JSON.pretty_generate( res )
       rescue JSON::GeneratorError
         {
-          response: 'Something is wrong with the JSON that you entered.',
+          response: "The response could not be parsed. Status: #{res.code}",
           time: 0
         }
       else
         {
-          response: response,
+          response: res_json,
           time: end_time - start_time
         }
       end
