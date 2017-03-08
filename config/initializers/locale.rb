2005706843eade5dc6bb67af49a20726a4a7eacb
James Milani <james.milani@gmail.com>
2017-03-08 11:25:35 -0800

    If no env is set, default to english.

--- config/initializers/locale.rb
+++ config/initializers/locale.rb
@@ -2,4 +2,4 @@
 I18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}')]
 
 # Set default locale to something other than :en
-I18n.default_locale = ENV['LANGUAGE']
+I18n.default_locale = ENV['LANGUAGE'] ||= 'en'
