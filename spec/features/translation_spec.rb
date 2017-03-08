c4fe243ef41e49e69ab5ec15a30aa00e5a5e1cd2
James Milani <james.milani@gmail.com>
2017-03-07 18:11:35 -0800

    Configuration details and test for translation.

---
+++ spec/features/translation_spec.rb
@@ -0,0 +1,15 @@
+describe 'Translation', :js do
+  let!( :owner ){ create :user                             }
+  let!( :role  ){ create :role, name: 'owner', user: owner }
+
+  before do
+    stub_identity_token
+    stub_identity_account_for owner.email
+    visit '/login/success?code=0123abc'
+  end
+
+  specify 'should show "skill" in German' do
+    visit '/skills'
+    expect( page ).to have_content '1 Fähigkeit'
+  end
+end
