65d8e7079ad5c9b67768a404cbf4b52c5653d68b
James Milani <james.milani@gmail.com>
2017-03-17 15:23:52 -0700

    Tests for type text.

--- spec/features/dialogs_spec.rb
+++ spec/features/dialogs_spec.rb
@@ -87,12 +87,12 @@ feature 'Dialogs', :js do
       within 'form.dialog' do
         within '.response-type-row-0'do
           select  'Text',                   from: 'response-type-select'
-          fill_in 'response_text',          with: 'abc def 123 10 9 8'
+          fill_in 'response_text_input',    with: 'abc def 123 10 9 8'
           fill_in 'response_trigger_input', with: 'some kind of trigger'
         end
 
-        select field.name,    from: 'unresolved-field'
-        select field.name,    from: 'awaiting-field'
+        select field.name, from: 'unresolved-field'
+        select field.name, from: 'awaiting-field'
       end
 
       click_button 'Create Dialog'
@@ -102,11 +102,55 @@ feature 'Dialogs', :js do
       expect( Dialog.count   ).to eq 1
       expect( Response.count ).to eq 1
 
-      expected_response_value = { 'response_text' => 'abc def 123 10 9 8' }.to_json
+      expected_response_value = { 'response_text_input' => 'abc def 123 10 9 8' }.to_json
 
       expect( Dialog.last.responses.first.response_type     ).to eq 'text'
       expect( Dialog.last.responses.first.response_trigger  ).to eq 'some kind of trigger'
       expect( Dialog.last.responses.first.response_value    ).to eq expected_response_value
+    end
+
+    specify 'Can save mulitple responses of type text' do
+      visit '/login/success?code=0123abc'
+      click_link 'Intents'
+
+      click_link 'Edit Details'
+      click_link 'Add Dialogs'
+
+      within 'form.dialog' do
+        within '.response-type-row-0'do
+          select  'Text',                   from: 'response-type-select'
+          fill_in 'response_text_input',    with: 'abc def 123 10 9 8'
+          fill_in 'response_trigger_input', with: 'some kind of trigger'
+
+          find('span.icon-plus').click
+        end
+
+        within '.response-type-row-1' do
+          select  'Text',                   from: 'response-type-select'
+          fill_in 'response_text_input',    with: 'crazy dancing ninjas'
+          fill_in 'response_trigger_input', with: 'the best trigger'
+        end
+
+        select field.name, from: 'unresolved-field'
+        select field.name, from: 'awaiting-field'
+      end
+
+      click_button 'Create Dialog'
+
+      sleep 0.5
+
+      expect( Dialog.count   ).to eq 1
+      expect( Response.count ).to eq 2
+
+      expected_response_1 = { 'response_text_input' => 'abc def 123 10 9 8' }.to_json
+      expected_response_2 = { 'response_text_input' => 'crazy dancing ninjas' }.to_json
+
+      expect( Dialog.last.responses.first.response_type     ).to eq 'text'
+      expect( Dialog.last.responses.first.response_trigger  ).to eq 'some kind of trigger'
+      expect( Dialog.last.responses.first.response_value    ).to eq expected_response_1
+      expect( Dialog.last.responses.last.response_type      ).to eq 'text'
+      expect( Dialog.last.responses.last.response_trigger   ).to eq 'the best trigger'
+      expect( Dialog.last.responses.last.response_value     ).to eq expected_response_2
     end
 
     specify 'Can save a response of type text w/options' do
