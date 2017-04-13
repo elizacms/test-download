6db9640e1d1f3cdcf42b1cda69e0180ff1f47865
James Milani <james.milani@gmail.com>
2017-04-13 11:39:51 -0700

    Spec for hidden uploader when not admin.

--- spec/features/intent_upload_spec.rb
+++ spec/features/intent_upload_spec.rb
@@ -1,4 +1,5 @@
-describe 'Intent Upload' do
+describe 'Intent Upload', :js do
+  let!( :user   ){ create :user                                         }
   let!( :admin  ){ create :user, email: 'admin@iamplus.com'             }
   let!( :skill  ){ create :skill                                        }
   let!( :role   ){ create :role, skill: nil, name: 'admin', user: admin }
@@ -9,7 +10,7 @@ describe 'Intent Upload' do
     stub_identity_account_for admin.email
   end
 
-  specify 'User can upload multiple intent files and the objects save in the DB.', :js do
+  specify 'User can upload multiple intent files and the objects save in the DB.' do
     visit '/login/success?code=0123abc'
     visit "/skills/#{skill.id}/intents/"
 
@@ -29,5 +30,24 @@ describe 'Intent Upload' do
     expect( Field.where(intent_id: some.id ).count ).to eq 3
 
     expect( some.mturk_response ).to eq "[\"crazy_town\"]"
+  end
+
+  describe 'Admin can see uploader' do
+    specify 'Success' do
+      visit '/login/success?code=0123abc'
+      visit "/skills/#{skill.id}/intents/"
+
+      expect( page ).to have_content 'Choose Files'
+    end
+
+    specify 'Non-admin cannot see the uploader' do
+      stub_identity_token
+      stub_identity_account_for user.email
+
+      visit '/login/success?code=0123abc'
+      visit "/skills/#{skill.id}/intents/"
+
+      expect( page ).to_not have_content 'Choose Files'
+    end
   end
 end
