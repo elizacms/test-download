ceb2c5cd4830c73ff83365c7184aaccad37465ba
Henry Szeto <henryszeto@gmail.com>
2017-04-21 15:54:16 -0700

    seed db with an user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+u = User.create(email: 'iamplusqa@gmail.com')
+Role.create(name:"admin", user_id: u.id)
+
 FieldDataType.create( name: 'Boolean'  )
 FieldDataType.create( name: 'Date'     )
 FieldDataType.create( name: 'DateTime' )
