a87ea18018bea04f7504b446c874fe623364d648
James Milani <james.milani@gmail.com>
2017-05-02 16:57:38 -0700

    Oops. Fix reversed logic for helper.

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,5 +1,5 @@
 module PagesHelper
   def class_for_build( build_result )
-    build_result.downcase =~ /fail/ ? 'success' : 'fail'
+    build_result.downcase =~ /fail/ ? 'fail' : 'success'
   end
 end
