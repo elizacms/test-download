7298a39285ffa00e953e9eee4b95357fd43ed84b
James Milani <james.milani@gmail.com>
2017-05-10 17:38:50 -0700

    WIP

--- app/models/intent.rb
+++ app/models/intent.rb
@@ -11,13 +11,20 @@ class Intent
   field :external_applications,  type:Array, default: []
   field :requires_authorization, type:Mongoid::Boolean
 
-  validates_presence_of   :name
-  validates_uniqueness_of :name, scope: :skill_id
+  validates_presence_of :name
+  validate :unique_name
 
-  before_save ->{ validate }
+  before_create -> { validate }
 
   def external_applications
     self.requires_authorization == false ? [] : self[:external_applications]
+  end
+
+  def unique_name
+    ap self.name
+    if File.exist?( "#{ENV['NLU_CMS_PERSISTENCE_PATH']}/intents/#{self.name}.json" )
+      errors.add(:name, "must be unique")
+    end
   end
 
   def save opts
@@ -26,14 +33,13 @@ class Intent
       description: self.description,
       mturk_response: self.mturk_response
     }.to_json
-    
-    File.write("#{ENV['NLU_CMS_PERSISTENCE_PATH']}/intents/#{self.name}.json", 
-               file_data)
+
+    File.write("#{ENV['NLU_CMS_PERSISTENCE_PATH']}/intents/#{self.name}.json", file_data)
 
     self.name = nil
     self.description = nil
     self.mturk_response = nil
-  
-    super validate:false
+
+    super( validate: false )
   end
 end
