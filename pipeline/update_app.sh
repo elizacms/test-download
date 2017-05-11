5255a71fd8adbe97e42c0bd1fb9281abc92ba276
George Malary <george.malary@iamplus.com>
2017-05-11 11:05:55 -0700

    update pipeline update app

--- pipeline/update_app.sh
+++ pipeline/update_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 if [ "${BRANCH}" = 'staging' ]; then
   cd ~/devops_dev/jenkins/eliza-cms-stg
-  ansible-playbook -i aws_hosts.ini eliza-cms-update.yml --extra-vars="{'branch_name':'${BRANCH}'}"
+  ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${BRANCH}'}"
 else
   echo "no server to update for branch name: ${BRANCH}"
 fi
