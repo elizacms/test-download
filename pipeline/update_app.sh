9eb8910a0883e65292dae0446b60103c6cd86c0e
George Malary <george.malary@iamplus.com>
2017-05-16 15:47:56 -0700

    update path of update app script

--- pipeline/update_app.sh
+++ pipeline/update_app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 if [ "${BRANCH}" = 'staging' ]; then
-  cd ~/devops_dev/jenkins/eliza-cms-stg
+  cd ~/devops_dev/live/eliza/stg
   ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${BRANCH}'}"
 else
   echo "no server to update for branch name: ${BRANCH}"
