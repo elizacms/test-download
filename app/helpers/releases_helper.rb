977399b5923f0fe5dd0242fae3721f658364ba81
Henry Szeto <henryszeto@gmail.com>
2017-06-13 16:14:06 -0700

    use Courier font for diff

--- app/helpers/releases_helper.rb
+++ app/helpers/releases_helper.rb
@@ -3,7 +3,10 @@ module ReleasesHelper
     old_formatted = json_pretty_for single_diff[:old]
     new_formatted = json_pretty_for single_diff[:new]
 
-    Diffy::SplitDiff.new(old_formatted, new_formatted, format: :html, context: 0)
+    Diffy::SplitDiff.new(old_formatted, new_formatted,
+                          format: :html,
+                          include_plus_and_minus_in_html: true,
+                          context: 0)
   end
 
   def json_pretty_for content
