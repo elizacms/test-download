e717e953a5e9c608c6f066a358825af0b5bd2608
George Malary <george.malary@iamplus.com>
2017-06-21 11:12:43 -0700

    update app sends commit id instead of branch name

--- pipeline/update_app.sh
+++ pipeline/update_app.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 if [ "${BRANCH}" = 'staging' ]; then
   cd ~/devops_dev/live/eliza/stg
-  ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${BRANCH}'}"
+  ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}'}"
 else
   echo "no server to update for branch name: ${BRANCH}"
 fi
--- pipeline/update_app_nlu.sh
+++ pipeline/update_app_nlu.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -ex
 if [ "${BRANCH}" = 'staging' ]; then
   cd ~/devops_dev/live/nlu-cms/prod
-  ansible-playbook nlu-cms-update.yml --extra-vars="{'branch_name':'${BRANCH}'}"
+  ansible-playbook nlu-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}'}"
 else
   echo "no server to update for branch name: ${BRANCH}"
 fi
