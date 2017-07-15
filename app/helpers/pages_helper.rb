384eb2477bc86a3db933acacf138c06dd75a9680
Henry Szeto <henryszeto@gmail.com>
2017-07-14 17:27:57 -0700

    NCMS-86 Show Release status on Release page

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -2,4 +2,10 @@ module PagesHelper
   def class_for_build( build_result )
     build_result.downcase =~ /fail/ ? 'fail' : 'success'
   end
+
+  def style_for_release_state( state )
+    return 'green' if state == 'approved'
+
+    return 'red'   if state == 'rejected'
+  end
 end
