fec9773a3e098a494e32958982cf44271334d90f
James Milani <james.milani@gmail.com>
2017-07-21 17:11:10 -0700

    NCMS-97 upload btn does not break when file is empty.

--- app/assets/javascripts/intents/training_data_uploader.js
+++ app/assets/javascripts/intents/training_data_uploader.js
@@ -1,10 +1,22 @@
 $(document).on('turbolinks:load', function(){
   $('input[type=file]').change(function(e){
-    $('.infoText').html($(this)[0].files[0].name);
+    $('.infoText').html($(this)[0].files[0].name).css('color', 'inherit');
   });
 
-  $('.uploadBtn').click(function(){
+  $('.uploadBtn').click(function(e){
+    e.preventDefault();
+
+    if ( $('#files').val() === '' ){
+      $('.infoText').html('You must attach a file.').css('color', 'red');
+      setTimeout(function(){
+        $('.infoText').html('').css('color', 'inherit');
+      }, 2500);
+
+      return;
+    }
+
     var fileName = $('.infoText').text();
     $('.infoText').html('Saved ' + fileName + '!').css('color', 'green');
+    $( '.trainingDataUpload' ).submit();
   });
 });
