4dea3a536891717b71e45ed03b2dd0b3598ebc4c
George Malary <george.malary@iamplus.com>
2017-08-03 11:22:59 -0400

    update cd directory to update apps since renamed folders in devops folder

--- pipeline/update_app_aneeda.sh
+++ pipeline/update_app_aneeda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 if [ "${BRANCH}" = 'master' ]; then
-  cd ~/devops_dev/live/nlu-cms/prod
+  cd ~/devops_dev/live/nlu-cms/eliza-prod
   ansible-playbook nlu-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}'}"
 else
   echo "no server to update for branch name: ${BRANCH}"
--- pipeline/update_app_eliza.sh
+++ pipeline/update_app_eliza.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 if [ "${BRANCH}" = 'staging' ]; then
-  cd ~/devops_dev/live/eliza/stg
+  cd ~/devops_dev/live/eliza/iamplus-stg
   ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}'}"
 elif [ "${BRANCH}" = 'master' ]; then
-  cd ~/devops_dev/live/eliza/prod
+  cd ~/devops_dev/live/eliza/iamplus-prod
   ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}'}"
 else
   echo "no server to update for branch name: ${BRANCH}"
