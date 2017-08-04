edfb8d2df8a9f5adaf8263745dca27976a268cc5
George Malary <george.malary@iamplus.com>
2017-08-04 12:57:59 -0400

    update devops paths

--- pipeline/update_app_aneeda.sh
+++ pipeline/update_app_aneeda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 if [ "${BRANCH}" = 'master' ]; then
-  cd ~/devops_dev/application/eliza-nlu-cms/eliza-prod
+  cd ~/devops_dev/applications/eliza_nlu_cms/eliza-prod
   ansible-playbook nlu-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}'}"
 else
   echo "no server to update for branch name: ${BRANCH}"
--- pipeline/update_app_eliza_voice.sh
+++ pipeline/update_app_eliza_voice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 if [ "${BRANCH}" = 'master' ]; then
-  cd ~/devops_dev/applications/eliza-nlu-cms/iamplus-voice
+  cd ~/devops_dev/applications/eliza_nlu_cms/iamplus-voice
   ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}'}"
 else
   echo "no server to update for branch name: ${BRANCH}"
