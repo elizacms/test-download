916845d46787cf6a420e2262e68d31d87be12871
George Malary <george.malary@iamplus.com>
2017-08-07 14:11:09 -0400

    update to deploy to eliza hosted instances

--- pipeline/update_app_eliza.sh
+++ pipeline/update_app_eliza.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 if [ "${BRANCH}" = 'staging' ]; then
-  cd ~/devops_dev/applications/eliza_nlu_cms/iamplus-stg
+  cd ~/devops_dev/applications/eliza_nlu_cms/eliza-stg
   ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}'}"
 elif [ "${BRANCH}" = 'master' ]; then
-  cd ~/devops_dev/applications/eliza_nlu_cms/iamplus-prod
+  cd ~/devops_dev/applications/eliza_nlu_cms/eliza-prod
   ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}'}"
 else
   echo "no server to update for branch name: ${BRANCH}"
