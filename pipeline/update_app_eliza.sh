74ca241bfdb5a632a01de5ed2d241f270e23ba76
George Malary <george.malary@iamplus.com>
2017-08-07 15:20:58 -0400

    add eliza_repo env var to pipeline shell

--- pipeline/update_app_eliza.sh
+++ pipeline/update_app_eliza.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -ex
 if [ "${BRANCH}" = 'staging' ]; then
   cd ~/devops_dev/applications/eliza_nlu_cms/eliza-stg
-  ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}'}"
+  ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}','eliza_repo':'iAmPlus/eliza-cms-stg'}"
 elif [ "${BRANCH}" = 'master' ]; then
   cd ~/devops_dev/applications/eliza_nlu_cms/eliza-prod
-  ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}'}"
+  ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}','eliza_repo':'iAmPlus/eliza-lp'}"
 else
   echo "no server to update for branch name: ${BRANCH}"
 fi
