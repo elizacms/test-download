2da1a1a50bf1e432c59d83b803a356f0ef25739c
George Malary <george.malary@iamplus.com>
2017-08-08 20:40:34 -0400

    fix path again grrr

--- pipeline/update_app_aneeda.sh
+++ pipeline/update_app_aneeda.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 if [ "${BRANCH}" = 'staging' ]; then
-  cd ~/devops_dev/applications/eliza_nlu_cms/iamplus-nlu-cms
+  cd ~/devops_dev/applications/eliza_nlu_cms/iamplus-nlu-cms/prod
   ansible-playbook nlu-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}'}"
 else
   echo "no server to update for branch name: ${BRANCH}"
