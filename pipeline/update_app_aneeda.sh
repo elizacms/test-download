83c41bf949fad588ef7e30a87c94919ec9071dfe
James Milani <james.milani@gmail.com>
2017-08-08 17:24:05 -0700

    Aneeda push on staging for test-queries ease.

--- pipeline/update_app_aneeda.sh
+++ pipeline/update_app_aneeda.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ex
-if [ "${BRANCH}" = 'master' ]; then
+if [ "${BRANCH}" = 'staging' ]; then
   cd ~/devops_dev/applications/eliza_nlu_cms/eliza-prod
   ansible-playbook nlu-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}'}"
 else
