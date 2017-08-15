89674eb2f65d1e96dbfc481525988e01df5a9bcf
James Milani <james.milani@gmail.com>
2017-08-15 15:26:04 -0700

    Remove test-queries from the code base.

--- app/assets/javascripts/test_queries/codemirror/codemirror.js
+++
@@ -1,5 +0,0 @@
-//= require codemirror
-//= require codemirror/modes/javascript
-//= require codemirror/addons/fold/foldcode
-//= require codemirror/addons/fold/foldgutter
-//= require codemirror/addons/fold/brace-fold
--- app/assets/javascripts/test_queries/manifest.js
+++
@@ -1,2 +0,0 @@
-//= require_tree ./codemirror
-//= require test_queries/test_queries.js
