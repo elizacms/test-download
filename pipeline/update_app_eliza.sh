f1ffad404993abe6165d6388add213c00e148c79
George Malary <george.malary@iamplus.com>
2017-08-25 14:41:00 -0400

    update eliza repo for prod to test repo

--- pipeline/update_app_eliza.sh
+++ pipeline/update_app_eliza.sh
@@ -4,7 +4,7 @@ if [ "${BRANCH}" = 'staging' ]; then
   ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}','eliza_repo':'iAmPlus/eliza-cms-stg'}"
 elif [ "${BRANCH}" = 'master' ]; then
   cd ~/devops_dev/applications/eliza_nlu_cms/eliza-prod
-  ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}','eliza_repo':'iAmPlus/eliza-lp'}"
+  ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}','eliza_repo':'iAmPlus/eliza-cms-prod-test'}"
 else
   echo "no server to update for branch name: ${BRANCH}"
 fi
