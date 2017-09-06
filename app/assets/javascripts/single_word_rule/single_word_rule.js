2e36b1712af0e38a97815d41e18f19fa963b7308
Henry Szeto <henryszeto@gmail.com>
2017-09-06 13:23:59 -0700

    working on single word rule view

--- app/assets/javascripts/single_word_rule/single_word_rule.js
+++ app/assets/javascripts/single_word_rule/single_word_rule.js
@@ -1,10 +1,62 @@
+var buildTable = function(data) {
+  data.forEach(function(d, index){
+    $('#singleWordTable').append( '<tr class="single_word_row" data-id="'+index+'">'
+                                +   '<td class="table-view">'
+                                +     d[0]
+                                +   '</td>'
+                                +   '<td class="table-view">'
+                                +     d[1].split("{{")[1].split("}}")[0].split(":")[1]
+                                +   '</td>'
+                                +   '<td class="table-view single-word-rule-padL100">'
+                                +     '<a href="#" class="edit-btn btn md grey">Edit</a>'
+                                +   '</td>'
+
+                                +   '<td class="input-view displayNone">'
+                                +     '<input type="text" class="single-word-rule-input width200" value="'+d[0]+'"/>'
+                                +   '</td>'
+                                +   '<td class="input-view displayNone">'
+                                +     '<select class="intents-options"></select>'
+                                +   '</td>'
+                                +   '<td class="input-view single-word-rule-padL100 displayNone">'
+                                +     '<a href="#" class="save-btn btn md black">Save</a>'
+                                +   '</td>'
+                                + '</tr>'
+                                );
+  });
+};
+
+var getData = function() {
+  $.ajax({
+    type: 'GET',
+    url: '/api/single_word_rules',
+    dataType: "json",
+    headers: {
+      'Accept': 'application/json'
+    }
+  })
+  .done(function(data){
+    buildTable(data);
+
+    getIntents();
+
+    addAction();
+
+    editAction();
+
+    saveAction();
+  })
+  .fail(function(err){
+    console.log( err );
+  });
+};
+
 var appendOptions = function(intents) {
   intents.forEach(function(intent){
     $('.intents-options').append('<option value="'+intent+'">'+intent+'</option>');
   });
 };
 
-var getAllIntents = function() {
+var getIntents = function() {
   $.ajax({
     type: 'GET',
     url: '/api/intents',
@@ -23,9 +75,10 @@ var getAllIntents = function() {
 var addAction = function() {
   $('.add-btn').click(function(e){
     e.preventDefault();
-    console.log( "Add this." );
     var new_word = $('.single-word-rule-input.add').val();
     var new_word_intent = $('select.intents-options.add').val();
+
+    console.log( "Add this." );
     console.log( new_word );
     console.log( new_word_intent );
   });
@@ -36,6 +89,10 @@ var editAction = function() {
     e.preventDefault();
 
     var this_row = $(this).closest('.single_word_row');
+
+    // Pre select dropdown menu
+    var current_intent = this_row.find('td')[1].innerHTML;
+    this_row.find('option[value="'+current_intent+'"]').attr("selected", "selected");
 
     // Reset all td
     $('.single_word_row td').each(function(){
@@ -63,9 +120,10 @@ var editAction = function() {
 var saveAction = function() {
   $('.save-btn').click(function(e){
     e.preventDefault();
-    console.log( "Save this." );
     var edit_word =  $(this).parents().eq(1).find('input').val();
     var edit_word_intent = $(this).parents().eq(1).find('select').val();
+
+    console.log( "Save this." );
     console.log( edit_word );
     console.log( edit_word_intent );
   });
@@ -73,11 +131,5 @@ var saveAction = function() {
 
 
 $(document).on('turbolinks:load', function() {
-  getAllIntents();
-
-  addAction();
-
-  editAction();
-
-  saveAction();
+  getData();
 });
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -32,23 +32,6 @@ class PagesController < ApplicationController
   end
 
   def single_word_rule
-    @data = [
-      {
-        id: "10",
-        word: "infodienste",
-        intent: {intent: "need_help"}
-      },
-      {
-        id: "20",
-        word: "kundendienst",
-        intent: {intent: "connect_to_agent"}
-      },
-      {
-        id: "30",
-        word: "Zeige meine Rechnung ",
-        intent: {intent: "billing_mybill"}
-      }
-    ]
   end
 
   private
