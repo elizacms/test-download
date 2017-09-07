7d7e417925b06019c071a5d444712e73a6f9346d
James Milani <james.milani@gmail.com>
2017-09-07 16:34:40 -0700

    fix bug and refactor

--- app/models/single_word_rule_file_manager.rb
+++ app/models/single_word_rule_file_manager.rb
@@ -2,29 +2,50 @@ class SingleWordRuleFileManager
   include FilePath
 
   def load_file
-    CSV.read( single_word_rule_file )
-  end
-
-  def insert text, intent_ref
-    CSV.open( single_word_rule_file, 'a' ) do |csv|
-      csv << [ text, intent_ref ]
+    CSV.foreach( single_word_rule_file, skip_blanks:true ).map do |row|
+      [row[0], row[1]]
     end
   end
 
-  def update row_num, text, intent_ref
-    temp_file = File.new( "#{de_language_path}/temp.csv", 'w' )
+  def insert text, intent_ref
+    size = CSV.open(single_word_rule_file, skip_blanks:true).count
 
-    CSV.open( temp_file, 'w' ) do |csv_out|
+    open_temp_file_and_loop_single_word_file( text, intent_ref, size, nil )
+
+    delete_original_and_rename
+  end
+
+  def update row_num, text, intent_ref
+    open_temp_file_and_loop_single_word_file( text, intent_ref, nil, row_num )
+
+    delete_original_and_rename
+  end
+
+
+  private
+
+  def new_temp_file
+    File.new( "#{de_language_path}/temp.csv", 'w' )
+  end
+
+  def delete_original_and_rename
+    File.delete( single_word_rule_file )
+    File.rename( "#{de_language_path}/temp.csv", single_word_rule_file )
+  end
+
+  def open_temp_file_and_loop_single_word_file( text, intent_ref, size=nil, row_num=nil )
+    CSV.open( new_temp_file, 'w' ) do |csv_out|
       CSV.foreach( single_word_rule_file ).with_index do |row, index|
-        if row_num.to_i == index
+        if row_num && row_num.to_i == index || size && size.to_i == index + 1
+          if size && size.to_i == index + 1
+            csv_out << row
+          end
+
           csv_out << [ text, intent_ref ]
         else
           csv_out << row
         end
       end
     end
-
-    File.delete( single_word_rule_file )
-    File.rename( "#{de_language_path}/temp.csv", "#{de_language_path}/german-intents-singleword-rules.csv" )
   end
 end
---
+++ spec/models/single_word_rule_file_manager_spec.rb
@@ -0,0 +1,81 @@
+describe SingleWordRuleFileManager,:focus do
+  before do
+    File.write(single_word_rule_file, File.read('spec/data-files/german-intents-singleword-rules.csv'))
+  end
+
+  describe '#load_file' do
+    it 'should succeed' do
+      expect( SingleWordRuleFileManager.new.load_file ).to eq [['bunny', '{{intent:fiver}}'],
+                                                               ['wings', '{{intent:kehar}}']]
+    end
+  end
+
+  describe '#insert' do
+    it 'should succeed' do
+      SingleWordRuleFileManager.new.insert( 'state', '{{intent:bunks}}' )
+
+      expect( SingleWordRuleFileManager.new.load_file ).to eq [['bunny', '{{intent:fiver}}'],
+                                                               ['wings', '{{intent:kehar}}'],
+                                                               ['state', '{{intent:bunks}}']]
+    end
+
+    it 'should succeed even with extra blank lines' do
+      File.write(single_word_rule_file,File.read('spec/data-files/singleword-rules-with-blank-lines.csv'))
+      SingleWordRuleFileManager.new.insert( 'state', '{{intent:bunks}}' )
+
+      expect( SingleWordRuleFileManager.new.load_file ).to eq [['bunny', '{{intent:fiver}}'],
+                                                               ['wings', '{{intent:kehar}}'],
+                                                               ['state', '{{intent:bunks}}']]
+    end
+  end
+
+  describe '#update' do
+    it 'should succeed' do
+      SingleWordRuleFileManager.new.update( '0', 'state', '{{intent:bunks}}' )
+
+      expect( SingleWordRuleFileManager.new.load_file ).to eq [['state', '{{intent:bunks}}'],
+                                                               ['wings', '{{intent:kehar}}']]
+    end
+
+    it 'should succeed when there are extra blank lines' do
+      File.write(single_word_rule_file,File.read('spec/data-files/singleword-rules-with-blank-lines.csv'))
+
+      SingleWordRuleFileManager.new.update( '1', 'state', '{{intent:bunks}}' )
+
+      expect( SingleWordRuleFileManager.new.load_file ).to eq [['bunny', '{{intent:fiver}}'],
+                                                               ['state', '{{intent:bunks}}']]
+    end
+  end
+
+  describe '#create and #update with extra lines' do
+    it 'should allow an insert and then an update and return correctly' do
+      File.write(single_word_rule_file,File.read('spec/data-files/singleword-rules-with-blank-lines.csv'))
+
+      SingleWordRuleFileManager.new.insert( 'state', '{{intent:bunks}}' )
+
+      expect( SingleWordRuleFileManager.new.load_file ).to eq [['bunny', '{{intent:fiver}}'],
+                                                               ['wings', '{{intent:kehar}}'],
+                                                               ['state', '{{intent:bunks}}']]
+
+      SingleWordRuleFileManager.new.update( '2', 'state', '{{intent:funks}}' )
+
+      expect( SingleWordRuleFileManager.new.load_file ).to eq [['bunny', '{{intent:fiver}}'],
+                                                               ['wings', '{{intent:kehar}}'],
+                                                               ['state', '{{intent:funks}}']]
+
+      SingleWordRuleFileManager.new.insert( 'great', '{{intent:funny_chicken}}')
+
+      expect( SingleWordRuleFileManager.new.load_file ).to eq [['bunny', '{{intent:fiver}}'],
+                                                               ['wings', '{{intent:kehar}}'],
+                                                               ['state', '{{intent:funks}}'],
+                                                               ['great', '{{intent:funny_chicken}}']]
+
+      SingleWordRuleFileManager.new.update( '3', 'aaaaa', '{{intent:55555}}')
+
+      expect( SingleWordRuleFileManager.new.load_file ).to eq [['bunny', '{{intent:fiver}}'],
+                                                               ['wings', '{{intent:kehar}}'],
+                                                               ['state', '{{intent:funks}}'],
+                                                               ['aaaaa', '{{intent:55555}}']]
+    end
+  end
+end
