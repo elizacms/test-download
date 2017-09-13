b67cfd7559e09a8fd67c23c45fd71230920c2ebe
George Malary <george.malary@iamplus.com>
2017-09-13 10:45:45 -0400

    devops: update directory of tf and ansible

--- pipeline/update_app_eliza.sh
+++ pipeline/update_app_eliza.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -ex
 if [ "${BRANCH}" = 'staging' ]; then
-  cd ~/devops_dev/applications/eliza_nlu_cms/eliza-stg
+  cd ~/devops_dev/infrastructure/legacy_applications/eliza_nlu_cms/eliza-stg
   ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}','eliza_repo':'iAmPlus/eliza-cms-stg'}"
 elif [ "${BRANCH}" = 'master' ]; then
-  cd ~/devops_dev/applications/eliza_nlu_cms/eliza-prod
+  cd ~/devops_dev/infrastructure/legacy_applications/eliza_nlu_cms/eliza-prod
   ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}','eliza_repo':'iAmPlus/eliza-cms-prod-test'}"
 else
   echo "no server to update for branch name: ${BRANCH}"
--- pipeline/update_app_eliza_voice.sh
+++ pipeline/update_app_eliza_voice.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -ex
 if [ "${BRANCH}" = 'master' ]; then
-  cd ~/devops_dev/applications/eliza_nlu_cms/eliza-voice
+  cd ~/devops_dev/infrastructure/legacy_applications/eliza_nlu_cms/eliza-voice
   ansible-playbook eliza-cms-update.yml --extra-vars="{'branch_name':'${COMMIT_ID}','eliza_repo':'iAmPlus/eliza_voice'}"
 else
   echo "no server to update for branch name: ${BRANCH}"
